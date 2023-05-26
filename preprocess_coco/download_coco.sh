aaac84bf4dd5b76709614a88d930eab84530128a
Enrique Piñero <epinerof@us.es>
2023-05-26 13:37:07 +0200

    Added preprocessing of downloaded annotations

--- preprocess_coco/download_coco.sh
+++ preprocess_coco/download_coco.sh
@@ -1,5 +1,11 @@
+#Uncomment this line to download train data too
 #wget http://images.cocodataset.org/zips/train2017.zip
 wget http://images.cocodataset.org/zips/val2017.zip
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip val2017.zip
 unzip annotations_trainval2017.zip
+mv annotations_trainval2017/annotations ./
+
+python3 preprocess_coco_person.py annotations/person_keypoints_val2017.json annotations/person_keypoints_val2017_modified.json 
+#Uncomment next line to preprocess training annotations too
+#python3 preprocess_coco_person.py annotations/person_keypoints_train2017.json annotations/person_keypoints_train2017_modified.json 
