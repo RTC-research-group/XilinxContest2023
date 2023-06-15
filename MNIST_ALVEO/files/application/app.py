1c9fc7325f03a64fefd804e56c55f8c62d313e1f
Pablo <pablo-aries-7@hotmail.com>
2023-06-15 12:45:48 +0200

    Añadido el batchSize por consola de comandos.

--- MNIST_ALVEO/files/application/app.py
+++ MNIST_ALVEO/files/application/app.py
@@ -4,7 +4,7 @@ from ThreadsHandler import ThreadsHandler, DisplayThread
 import cv2
 import numpy as np
 
-def app(source: str, threads: int, modelFile: str, targetFPS: int, batchSize: int = 4):
+def app(source: str, threads: int, modelFile: str, targetFPS: int, batchSize: int = 1):
     # 1) The ThreadsHandler is initialized:
     handler: ThreadsHandler = ThreadsHandler(threads, modelFile)
 
@@ -57,7 +57,7 @@ def main():
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument('-s', '--source', type=str, default='0', help='Input video device ID or path to video. Default is 0 (webcam)')
-    ap.add_argument('-t', '--threads', type=int, default=4, help='Number of threads. Default is 1')
+    ap.add_argument('-t', '--threads', type=int, default=1, help='Number of threads. Default is 1')
     # ap.add_argument('-m', '--model', type=str, default='../../../vitis/compiled_pytorch_xmodel/Sequential_int.xmodel',
     #                help='Path of xmodel. Default is ../../../vitis/compiled_pytorch_xmodel/Sequential_int.xmodel')
     ap.add_argument('-m', '--model', type=str, default='../../../trt_pose/models/resnet18_baseline_att_224x224_A_epoch_249.pth',
@@ -67,6 +67,7 @@ def main():
     #                help='Path of xmodel. Default is ../../../trt_pose/models/densenet121_baseline_att_256x256_B_epoch_160.pth')
 
     ap.add_argument('-f', '--fps', type=int, default=24, help='Target FPS. Default is 24')
+    ap.add_argument('-b', '--batchSize', type=int, default=1, help='Batch size. Default is 1')
 
     args = ap.parse_args()
 
@@ -75,8 +76,9 @@ def main():
     print(' --threads   : ', args.threads)
     print(' --model     : ', args.model)
     print(' --fps     : ', args.fps)
+    print(' --batchSize     : ', args.batchSize)
 
-    app(args.source, args.threads, args.model, args.fps)
+    app(args.source, args.threads, args.model, args.fps, args.batchSize)
 
 
 if __name__ == '__main__':
