4d78a7734a51d687094d7fa1a10ac329326d10bb
Enrique Piñero <epinerof@us.es>
2023-06-29 00:05:54 +0200

    Added options to use tanh cpu implementation

--- compile/compile_command.sh
+++ compile/compile_command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir compile_result
-python3 compile/vai_c_xir_cust_pytorch.py -x quantize_result/Sequential_int.xmodel -a /opt/vitis_ai/compiler/arch/DPUCAHX8H/U280/arch.json -o compile_result -n trt_pose
+python3 compile/vai_c_xir_cust_pytorch.py -x quantize_result/Sequential_int.xmodel -a /opt/vitis_ai/compiler/arch/DPUCAHX8H/U280/arch.json -o compile_result -n trt_pose --options '{"plugins": "vart_op_imp_tanh"}'
